00327563ad75815b02e141ddb5b4a412086fba3d
Richard Mulu <richard.ndisya@strathmore.edu>
2024-02-17 18:31:52 +0300

    first commit

---
+++ src/views/CarView.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { useRoute, RouterView, useRouter } from "vue-router";
+import cars from "../data/cars.json";
+
+const route = useRoute();
+const router = useRouter();
+
+const carId = parseInt(route.params.id);
+
+const car = cars.find((c) => c.id === parseInt(route.params.id));
+
+const showContact = () => {
+  if (carId === 4) {
+    return;
+  }
+
+  return router.push(`/cars/${car.id}/contact`)
+};
+</script>
+
+<template>
+  <div v-if="car">
+    <h1>Car View</h1>
+    <p>{{ car.name }}</p>
+    <p>{{ car.year }}</p>
+    <p>{{ car.price }}</p>
+    <button @click="showContact">
+      Click for contact
+    </button>
+    <RouterView />
+  </div>
+  <div v-else>
+    <h1>Car not found</h1>
+  </div>
+</template>
